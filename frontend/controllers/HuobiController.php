434ba86d883ad3f025466bbf866056226f2764c0
MiiCoPCJ <EngMoreSite@outlook.com>
2019-05-24 16:47:11 +0800

    添加huobi api

--- frontend/controllers/HuobiController.php
+++ frontend/controllers/HuobiController.php
@@ -3,13 +3,47 @@
 namespace frontend\controllers;
 use frontend\data\UserContext;
 use hzfw\web\Controller;
+use hzfw\web\Common;
 
 class HuobiController extends Controller
 {
     //ws
-    public function WS()
+    public function Pro(string $period,int $size)
     {
-        return $this->View('huobi');
+      $func = new Common();
+
+      $arr = array();
+      $arr['period'] = $period;
+      $arr['size'] = $size;
+
+      $url = "https://api.huobi.pro/market/history/kline?period=$period&size=$size&symbol=btcusdt";
+      $api = $func->request_get($url);
+      $api = json_decode($api,true);
+
+      $huobi = array();
+      foreach($api as $key=>$value){
+        $time = date('Y-m-d H:i:s',$value['id']);
+        $open = (float)$value['open'];
+        $high = (float)$value['high'];
+        $low = (float)$value['low'];
+        $close = (float)$value['close'];
+        $volume = (string)$value['vol'];
+
+        $p = -1;
+        if($open<$close){
+          $p = 1;
+        }
+
+        $huobi[] = [$time,$open,$high,$low,$close,$volume,$p];
+      }
+
+      $arr['huobi'] = [];
+      if(!empty($huobi)){
+        $arr['huobi'] = json_encode($huobi);
+      }
+
+
+      return $this->View('huobi',$arr);
     }
 
 
--- frontend/views/Huobi/huobi.php
+++ frontend/views/Huobi/huobi.php
@@ -1,24 +1,163 @@
-<div>
-	<div>huobi</div>
+<script src="/js/jquery.min.js"></script>
+<script type="text/javascript" src="/js/echarts.min.js"></script>
+<link rel="stylesheet" type="text/css" href="/css/style.css">
+
+<div class="wrap">
+	<div id="container" style="height: 100%"></div>
 </div>
 
-<script>
- ws = new WebSocket('wss://api.huobi.pro/ws');
- ws.onopen = function (ev) {
-		 console.log('onopen');
-		 console.log(ev);
- };
- ws.onmessage = function (ev) {
-		 console.log('onmessage');
-		 console.log(ev);
- }
+<script type="text/javascript">
+var dom = document.getElementById("container");
+var myChart = echarts.init(dom);
+var app = {};
+option = null;
+var upColor = '#ec0000';
+var upBorderColor = '#8A0000';
+var downColor = '#00da3c';
+var downBorderColor = '#008F28';
 
- ws.onclose = function (ev) {
-		 console.log('onclose');
-		 console.log(ev);
- };
- ws.onerror = function (ev) {
-		 console.log('onerror');
-		 console.log(ev);
- };
+var dataCount = 5;
+var data = <?php echo $model['huobi'] ?>;
+
+var option = {
+    dataset: {
+        source: data
+    },
+    title: {
+        text: 'Huobi Data'
+    },
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'line'
+        }
+    },
+    toolbox: {
+        feature: {
+            dataZoom: {
+                yAxisIndex: false
+            },
+        }
+    },
+    grid: [
+        {
+            left: '10%',
+            right: '10%',
+            bottom: 200
+        },
+        {
+            left: '10%',
+            right: '10%',
+            height: 80,
+            bottom: 80
+        }
+    ],
+    xAxis: [
+        {
+            type: 'category',
+            scale: true,
+            boundaryGap : false,
+            inverse: true,
+            axisLine: {onZero: false},
+            splitLine: {show: false},
+            splitNumber: 20,
+            min: 'dataMin',
+            max: 'dataMax'
+        },
+        {
+            type: 'category',
+            gridIndex: 1,
+            scale: true,
+            boundaryGap : false,
+            axisLine: {onZero: false},
+            axisTick: {show: false},
+            splitLine: {show: false},
+            axisLabel: {show: false},
+            splitNumber: 20,
+            min: 'dataMin',
+            max: 'dataMax'
+        }
+    ],
+    yAxis: [
+        {
+            scale: true,
+            splitArea: {
+                show: true
+            }
+        },
+        {
+            scale: true,
+            gridIndex: 1,
+            splitNumber: 2,
+            axisLabel: {show: false},
+            axisLine: {show: false},
+            axisTick: {show: false},
+            splitLine: {show: false}
+        }
+    ],
+    dataZoom: [
+        {
+            type: 'inside',
+            xAxisIndex: [0, 1],
+            start: 10,
+            end: 100
+        },
+        {
+            show: true,
+            xAxisIndex: [0, 1],
+            type: 'slider',
+            bottom: 10,
+            start: 10,
+            end: 100,
+            handleIcon: 'M10.7,11.9H9.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4h1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+            handleSize: '105%'
+        }
+    ],
+    visualMap: {
+        show: false,
+        seriesIndex: 1,
+        dimension: 6,
+        pieces: [{
+            value: 1,
+            color: upColor
+        }, {
+            value: -1,
+            color: downColor
+        }]
+    },
+    series: [
+        {
+            type: 'candlestick',
+            itemStyle: {
+                color: upColor,
+                color0: downColor,
+                borderColor: upBorderColor,
+                borderColor0: downBorderColor
+            },
+            encode: {
+                x: 0,
+                y: [1, 4, 3, 2]
+            }
+        },
+        {
+            name: 'Volumn',
+            type: 'bar',
+            xAxisIndex: 1,
+            yAxisIndex: 1,
+            itemStyle: {
+                color: '#7fbe9e'
+            },
+            large: true,
+            encode: {
+                x: 0,
+                y: 5
+            }
+        }
+    ]
+};
+
+
+if (option && typeof option === "object") {
+    myChart.setOption(option, true);
+}
 </script>
